adf5078fecf84fa171a06c23b85d82b5d3ee2fa7
Miguel <miguel.saraiva@tecnico.ulisboa.pt>
2020-09-23 16:15:23 +0100

    final version

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-const express = require('express')
-
-require('./db/mongoose')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-//in go filecan we separete like this? how to make complex requests?
-// check http types put patch get post ...
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
